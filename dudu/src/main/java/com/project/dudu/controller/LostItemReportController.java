196acedec63af7c252eb38e73042c3673eb11a04
geonugang <cooper091677@gmail.com>
2024-11-07 14:56:45 +0900

    삭제 및 업데이트 메서드 작성

--- dudu/src/main/java/com/project/dudu/controller/LostItemReportController.java
+++ dudu/src/main/java/com/project/dudu/controller/LostItemReportController.java
@@ -35,14 +35,40 @@ public class LostItemReportController {
     }
 
     // 특정 ID로 분실물 신고 조회
-    @GetMapping("/{id}")
+    @GetMapping("/{id}")  //차후 수정
     public ResponseEntity<LostItemReportDto> getReportById(@PathVariable Long id) {
         // Optional 처리에 맞게 수정
         Optional<LostItemReportDto> lostItemReport = lostItemReportService.getReportById(id);
 
-        // Optional의 존재 여부를 체크하고 ResponseEntity를 반환
+        // Optional의 존재 여부를 체크하고 ResponseEntity를 반환  이 부분 조금 애매
         return lostItemReport
                 .map(report -> new ResponseEntity<>(report, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
+
+    // 특정 ID의 분실물 신고 업데이트
+    @PutMapping("/{id}") //차후 수정
+    public ResponseEntity<LostItemReportDto> updateReport(@PathVariable Long id, @RequestBody LostItemReportDto lostItemReportDto) {
+        LostItemReportDto updatedReport = lostItemReportService.updateReport(id, lostItemReportDto);
+        if (updatedReport != null) {
+            return new ResponseEntity<>(updatedReport, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // 특정 ID의 분실물 신고를 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReport(@PathVariable Long id) {
+        try {
+            lostItemReportService.deleteReport(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+
 }

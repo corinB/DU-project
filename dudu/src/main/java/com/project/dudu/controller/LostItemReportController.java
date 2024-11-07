61e2ba5dd9ce09ea84e9c9fa279ee8efa11e98ac
geonugang <cooper091677@gmail.com>
2024-11-07 14:47:53 +0900

    모든 분실물 신고 조회 메서드 작성

--- dudu/src/main/java/com/project/dudu/controller/LostItemReportController.java
+++ dudu/src/main/java/com/project/dudu/controller/LostItemReportController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/LostItems")
 public class LostItemReportController {
@@ -19,8 +21,15 @@ public class LostItemReportController {
 
     // 분실물 신고 생성 메서드
     @PostMapping
-    public ResponseEntity<LostItemReportDto> createLostItemReport(@RequestBody LostItemReportDto lostItemReportDto) {
+    public ResponseEntity<LostItemReportDto> createReport(@RequestBody LostItemReportDto lostItemReportDto) {
         LostItemReportDto createReport = lostItemReportService.createReport(lostItemReportDto);
         return new ResponseEntity<>(createReport, HttpStatus.CREATED);
+    }
+
+    // 모든 분실물 신고 조회
+    @GetMapping
+    public ResponseEntity<List<LostItemReportDto>> getAllReports() {
+        List<LostItemReportDto> lostItems = lostItemReportService.getAllReports();
+        return new ResponseEntity<>(lostItems, HttpStatus.OK);
     }
 }

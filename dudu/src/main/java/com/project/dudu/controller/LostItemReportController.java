0d34bbf96fd1514f12331db62e1ed4f14b86de1a
geonugang <cooper091677@gmail.com>
2024-11-07 14:52:30 +0900

    특정 ID로 분실물 신고 조회 메서드 작성

--- dudu/src/main/java/com/project/dudu/controller/LostItemReportController.java
+++ dudu/src/main/java/com/project/dudu/controller/LostItemReportController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/LostItems")
@@ -31,5 +32,17 @@ public class LostItemReportController {
     public ResponseEntity<List<LostItemReportDto>> getAllReports() {
         List<LostItemReportDto> lostItems = lostItemReportService.getAllReports();
         return new ResponseEntity<>(lostItems, HttpStatus.OK);
+    }
+
+    // 특정 ID로 분실물 신고 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<LostItemReportDto> getReportById(@PathVariable Long id) {
+        // Optional 처리에 맞게 수정
+        Optional<LostItemReportDto> lostItemReport = lostItemReportService.getReportById(id);
+
+        // Optional의 존재 여부를 체크하고 ResponseEntity를 반환
+        return lostItemReport
+                .map(report -> new ResponseEntity<>(report, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }

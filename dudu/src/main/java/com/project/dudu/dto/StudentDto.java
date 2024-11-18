b108f6f3c60948f31761f9054b4c29dc875baac8
geonugang <cooper091677@gmail.com>
2024-11-18 10:39:40 +0900

    StudentDto Getter, Setter 수정

--- dudu/src/main/java/com/project/dudu/dto/StudentDto.java
+++ dudu/src/main/java/com/project/dudu/dto/StudentDto.java
@@ -6,29 +6,23 @@ import com.project.dudu.enums.ReservationType;
 import lombok.*;
 import org.hibernate.annotations.Collate;
 
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
 public class StudentDto {
     @JsonProperty(value = "student_id")
-    @Setter
-    @Getter
     private Long studentId;
 
     @JsonProperty(value = "password")
-    @Setter
-    @Getter
     private String password;
 
     @JsonProperty(value = "student_name")
-    @Setter
-    @Getter
     private String studentName;
 
     @JsonProperty(value = "department")
-    @Setter
-    @Getter
     private String department;
 
     public StudentDto(long studentId, String studentName, String password) {

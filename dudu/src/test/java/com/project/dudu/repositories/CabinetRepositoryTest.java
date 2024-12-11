b2ca2b9c33baf7e22060db2d7be9ae33f51c96bc
geonugang <cooper091677@gmail.com>
2024-12-11 13:29:05 +0900

    add LostItem Status

--- dudu/src/test/java/com/project/dudu/repositories/CabinetRepositoryTest.java
+++ dudu/src/test/java/com/project/dudu/repositories/CabinetRepositoryTest.java
@@ -1,3 +1,56 @@
+//package com.project.dudu.repositories;
+//
+//import com.project.dudu.entities.CabinetEntity;
+//import com.project.dudu.enums.Colleges;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.annotation.Rollback;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.time.LocalDateTime;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//class CabinetRepositoryTest {
+//    @Autowired
+//    CabinetRepository cabinetRepository;
+//
+//    @Test
+//    @Transactional
+//    @Rollback(value = false)
+//    public void addCabinet() {
+//        for (int i = 0; i < 10; i++) {
+//            var cabinet = CabinetEntity.builder()
+//                    // .cabinetId(i) // 제거: 자동 생성되도록 설정
+//                    .department(Colleges.Engineering)
+//                    .createAt(LocalDateTime.now()) // 필요한 경우 추가
+//                    .updateAt(LocalDateTime.now()) // 필요한 경우 추가
+//                    .build();
+//            cabinetRepository.save(cabinet);
+//        }
+//    }
+//
+//    @Test
+//    @Transactional
+//    @Rollback(value = false)
+//    public void save220Cabinet() {
+//        Colleges[] colleges = Colleges.values();
+//        for (Colleges college : colleges) {
+//            for (int i = 0; i < 20; i++) {
+//                var cabinet = CabinetEntity.builder()
+//                        // .cabinetId(i) // 제거: 자동 생성되도록 설정
+//                        .department(college)
+//                        .createAt(LocalDateTime.now()) // 필요한 경우 추가
+//                        .updateAt(LocalDateTime.now()) // 필요한 경우 추가
+//                        .build();
+//                cabinetRepository.save(cabinet);
+//            }
+//        }
+//    }
+//}
+
 package com.project.dudu.repositories;
 
 import com.project.dudu.entities.CabinetEntity;
@@ -5,48 +58,32 @@ import com.project.dudu.enums.Colleges;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CabinetRepositoryTest {
+
     @Autowired
-    CabinetRepository cabinetRepository;
+    private CabinetRepository cabinetRepository;
 
     @Test
-    @Transactional
-    @Rollback(value = false)
-    public void addCabinet() {
-        for (int i = 0; i < 10; i++) {
-            var cabinet = CabinetEntity.builder()
-                    // .cabinetId(i) // 제거: 자동 생성되도록 설정
-                    .department(Colleges.Engineering)
-                    .createAt(LocalDateTime.now()) // 필요한 경우 추가
-                    .updateAt(LocalDateTime.now()) // 필요한 경우 추가
+    void insertCabinets() {
+        IntStream.rangeClosed(1, 30).forEach(i -> {
+            CabinetEntity cabinet = CabinetEntity.builder()
+                    .department(Colleges.values()[i % Colleges.values().length])  // 부서 순환 입력
+                    .createAt(LocalDateTime.now())
+                    .updateAt(LocalDateTime.now())
                     .build();
-            cabinetRepository.save(cabinet);
-        }
-    }
 
-    @Test
-    @Transactional
-    @Rollback(value = false)
-    public void save220Cabinet() {
-        Colleges[] colleges = Colleges.values();
-        for (Colleges college : colleges) {
-            for (int i = 0; i < 20; i++) {
-                var cabinet = CabinetEntity.builder()
-                        // .cabinetId(i) // 제거: 자동 생성되도록 설정
-                        .department(college)
-                        .createAt(LocalDateTime.now()) // 필요한 경우 추가
-                        .updateAt(LocalDateTime.now()) // 필요한 경우 추가
-                        .build();
-                cabinetRepository.save(cabinet);
-            }
-        }
+            cabinetRepository.save(cabinet);
+        });
+
+        // 저장 확인
+        long count = cabinetRepository.count();
+        assertThat(count).isGreaterThanOrEqualTo(30);
     }
 }
